fa54008855a03d928d1ee85f25742f5fb4522149
neitralitet <vladimirmanna@gmail.com>
2020-12-24 12:31:56 +0500

    departments Serializable

--- src/main/java/com/mann/dto/Department.java
+++ src/main/java/com/mann/dto/Department.java
@@ -1,11 +1,13 @@
 package com.mann.dto;
 
+import java.io.Serializable;
 import java.util.Set;
 
 /**
  * @author Vladimir Mann
  */
-public class Department {
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Integer id;
 
 
