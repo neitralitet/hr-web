4413b9191fa67d06555af4615d886f2222075d40
neitralitet <vladimirmanna@gmail.com>
2020-12-24 05:16:43 +0500

    path

--- src/main/java/com/mann/controller/RestClient.java
+++ src/main/java/com/mann/controller/RestClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import com.mann.controller.Employee;
+import com.mann.dto.Employee;
 import javax.annotation.PostConstruct;
 
 /**
